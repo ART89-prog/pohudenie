5b6fbf79308b9380828f99357c1224094140a289
ART89-prog <dykpgilkom@mail.ru>
2022-04-28 21:20:33 +0300

    Доделал

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ $(() => {
 	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
 
 
+
+	$('.information_box-right a').click(function(event) {
+		event.preventDefault();
+		$('.information_block').css('display', 'block');
+	  });
+
+
 })
 
 
